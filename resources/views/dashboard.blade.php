207171d5a1573770ddb55f0533b010409c86b12f
Omar Elewa <omarelewa25@gmail.com>
2024-06-07 16:33:14 +0300

    Add a button for diagnosing new image

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,10 +7,15 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+            <div class="p-6 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="text-gray-900 dark:text-gray-100">
+                    {{ __("Welcome to AlRazi for Mayo Clinic Diagnosis") }}
                 </div>
+                <button class="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    <a href="{{ route('diagnose') }}">
+                        {{ __("Diagnose New X-ray Image") }}
+                    </a>
+                </button>
             </div>
         </div>
     </div>

cec3b5181e7b31f816a6440395178e65e9dc8f49
Omar Elewa <omarelewa25@gmail.com>
2024-06-27 14:14:58 +0300

    to be continued

--- app/Livewire/Pages/ImageDiagnose/Main.php
+++ app/Livewire/Pages/ImageDiagnose/Main.php
@@ -35,113 +35,75 @@ class Main extends Component
     #[Validate('mimes:dcm', message: 'Please provide a valid DICOM file')]
     public $file;
 
-    public $images = [];
+    public $diagnoseImages = [];
     public $sourceImg;
     public $observations = [];
+    public $payloadObservations = [];
+    public $dicomData;
+    public $response;
 
-    public function mount()
-    {
-        $data = $this->getSampleResponse();
-        // $this->setDiagnoseResponseData($data);
-        $this->observations = $this->getObservations($data);
-    }
+    // public function mount()
+    // {
+    //     $data = $this->getSampleResponse();
+    //     $this->setObservations($data);
+    // }
 
-    public function render()
-    {
-        return view('livewire.pages.image-diagnose.main');
-    }
-
-    public function processDiagnosis()
-    {
-        $this->validate();
-        $this->processFile();
-
-        // $payload = $this->getImageDiagnosePayload();
-        // $data = Http::post('https://api.example.com/diagnose', $payload);
-
-        // Sample response
-        // $data = $this->getSampleResponse();
-        // $this->setDiagnoseResponseData($data);
-    }
-
-    private function getImageDiagnosePayload()
-    {
-        // Prepare the payload to send to the API
-    }
-
+    // To be removed
     private function getSampleResponse()
     {
         $response = Storage::get('sample-response.json');
         return json_decode($response, true);
     }
 
-    private function setDiagnoseResponseData($data)
+    public function processDiagnosis()
     {
-        $this->images = collect($data)
+        $this->validate();
+        $this->processFile();
+    }
+
+    private function processFile()
+    {
+        $response = $this->getDiagnosis();
+        $this->setDiagnoseData($response);
+    }
+
+    private function getDiagnosis()
+    {
+        $response = Http::timeout(10000)
+            ->post(env('PROCESS_SERVER') . '/analyze', ['image' => $this->getSourceImageFromDicomData()]);
+        return $response->json();
+    }
+
+    private function setDiagnoseData($data)
+    {
+        $this->setDiagnoseImages($data);
+        $this->setPayloadObservations($data);
+        $this->setObservations($data);
+    }
+
+    private function setDiagnoseImages($data)
+    {
+        $this->diagnoseImages = collect($data)
             ->filter(fn ($value, $key) => Str::contains($key, ['img', 'all_layers']))
             ->mapWithKeys(fn ($image, $key) => [
                 trim(Str::remove('Img', Str::headline($key))) => [
-                    'url' => "data:image/png;base64,$image",
+                    'url' => "data:image/png;base64,{$this->setImageSize($image)}",
                     'visibility' => true,
                 ]
             ])
             ->except('Source')
             ->toArray();
-        // $this->observations = $this->getObservations($data);
-        // $this->sourceImg = [
-        //     'url' => "data:image/png;base64,{$this->setImageSize($data['source_img'])}",
-        //     'visibility' => true,
-        // ];
     }
 
-    public function allHidden()
+    private function setPayloadObservations($data)
     {
-        return collect($this->images)->every(fn ($image) => ! $image['visibility']);
+        $this->payloadObservations = collect($data)->reject(fn ($value, $key) => Str::contains($key, ['img', 'all_layers']))
+            ->toArray();
     }
 
-    public function toggleAllVisibility()
+    private function setObservations($data)
     {
-        $visibility = $this->allHidden();
-
-        $this->images = collect($this->images)->map(function ($image) use ($visibility) {
-            $image['visibility'] = $visibility;
-            return $image;
-        })->toArray();
-    }
-
-    public function toggleVisibility($key)
-    {
-        $this->images[$key]['visibility'] = ! $this->images[$key]['visibility'];
-    }
-
-    private function processFile()
-    {
-        $fileExtension = $this->file->getClientOriginalExtension();
-        $payload = match ($fileExtension) {
-            'dcm' => $this->processDicomFile(),
-            default => $this->processImageFile(),
-        };
-        $this->setSourceImage($payload);
-        $response = $this->getResponse($payload);
-        $this->setDiagnoseResponseData($response);
-
-    }
-
-    private function processDicomFile()
-    {
-        $this->file->storeAs('dicom', $this->file->hashName(), 'shared');
-        $response = Http::acceptJson()->get(sprintf("%s/%s", config('app.dicom_server'), $this->file->hashName()));
-        Storage::disk('shared')->delete("dicom/{$this->file->hashName()}");
-        return $response->json();
-    }
-
-    private function processImageFile()
-    {
-    }
-
-    private function getObservations($data)
-    {
-        return collect($data)->reject(fn ($value, $key) => Str::contains($key, ['img', 'all_layers']))
+        $this->observations = collect($data)->reject(fn ($value, $key) => Str::contains($key, ['img', 'all_layers']))
             ->mapWithKeys(function ($value, $key) {
                 $return = [];
                 foreach ($value as $k => $v) {
@@ -155,6 +117,34 @@ class Main extends Component
             });
     }
 
+    public function allHidden()
+    {
+        return collect($this->diagnoseImages)->every(fn ($image) => ! $image['visibility']);
+    }
+
+    public function toggleAllVisibility()
+    {
+        $visibility = $this->allHidden();
+
+        $this->diagnoseImages = collect($this->diagnoseImages)->map(function ($image) use ($visibility) {
+            $image['visibility'] = $visibility;
+            return $image;
+        })->toArray();
+    }
+
+    public function toggleVisibility($key)
+    {
+        $this->diagnoseImages[$key]['visibility'] = ! $this->diagnoseImages[$key]['visibility'];
+    }
+
+    private function parseDicomFile()
+    {
+        $this->file->storeAs('dicom', $this->file->hashName(), 'shared');
+        $response = Http::acceptJson()->get(sprintf("%s/%s", config('app.dicom_server'), $this->file->hashName()));
+        Storage::disk('shared')->delete("dicom/{$this->file->hashName()}");
+        return $response->json();
+    }
+
     private function formatObsArray($array)
     {
         return collect($array)->map(function ($value, $key) {
@@ -162,23 +152,16 @@ class Main extends Component
         })->implode('');
     }
 
-    private function getResponse($payload)
+    private function getSourceImageFromDicomData()
     {
-        $response = Http::timeout(10000)->post(env('PROCESS_SERVER'), ['image' => $this->getImageFromPayload($payload)]);
-        return $response->json();
-    }
-
-    private function getImageFromPayload($payload)
-    {
-        $image = collect($payload)
+        return collect($this->dicomData)
             ->firstOrFail(fn($value, $key) => Str::contains($key, 'image'));
-        return $this->setImageSize($image);
     }
 
-    private function setSourceImage($payload)
+    private function setSourceImage()
     {
         $this->sourceImg = [
-            'url' => "data:image/png;base64,{$this->getImageFromPayload($payload)}",
+            'url' => "data:image/png;base64,{$this->setImageSize($this->getSourceImageFromDicomData())}",
             'visibility' => true,
         ];
     }
@@ -197,6 +180,29 @@ class Main extends Component
         imagepng($newImage);
         $image = ob_get_contents();
         ob_end_clean();
+        $this->sourceImg['width'] = $newWidth;
+        $this->sourceImg['height'] = $newHeight;
         return base64_encode($image);
+    }
+
+    public function updatedFile()
+    {
+        $this->validateOnly('file');
+        $this->dicomData = match ($this->file->getClientOriginalExtension()) {
+            'dcm' => $this->parseDicomFile(),
+            default => $this->parseDicomFile(),
+        };
+        $this->setSourceImage();
+    }
+
+    public function generateReport()
+    {
+        $response = Http::timeout(10000)
+            ->post(env('PROCESS_SERVER') . '/report', [
+                'result' => $this->payloadObservations,
+            ]);
+        $fileContents = $response->body();
+        Storage::disk('local')->put('report.pdf', $fileContents);
+        return response()->download(storage_path('app/report.pdf'));
     }
 }

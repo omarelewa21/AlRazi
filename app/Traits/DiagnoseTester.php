9087a112bf3da95446585b8c0f6687c885b245f5
Omar Elewa <omarelewa25@gmail.com>
2024-07-09 16:13:18 +0300

    make the tester

--- app/Traits/DiagnoseTester.php
+++ app/Traits/DiagnoseTester.php
@@ -8,12 +8,33 @@ trait DiagnoseTester
 {
     public function mount()
     {
-        $this->dicomData = $this->getDicomData();
-        $data = $this->getDiagnoseData();
-        $this->setSourceImage();
-        $this->setDiagnoseImages($data);
-        $this->setObservations($data);
-        $this->setReport();
+        $diagnoseData = $this->getTestDiagnoseData();
+        $this->setTestSourceImages($diagnoseData);
+        $this->setDiagnoseData($diagnoseData);
+    }
+
+    protected function getTestDiagnoseData()
+    {
+        $files = Storage::files('diagnose_test_data');
+        $jsonData = [];
+
+        foreach ($files as $index => $file) {
+            if (pathinfo($file, PATHINFO_EXTENSION) === 'json') {
+                $jsonData['image_' . $index+1] = json_decode(Storage::get($file), true);
+            }
+        }
+
+        return $jsonData;
+    }
+
+    protected function setTestSourceImages($data)
+    {
+        foreach ($data as $key => $item) {
+            $this->sourceImgs[$key] = [
+                'url' => "data:image/png;base64,{$this->setImageSize($item['source_img'])}",
+                'visibility' => true,
+            ];
+        }
     }
 
     private function getDicomData()

0e9396c1c94794aa7c6ea243b2cf5af9a18429c9
Omar Elewa <omarelewa25@gmail.com>
2024-07-23 18:51:28 +0300

    modify report job

--- app/Jobs/GenerateReport.php
+++ app/Jobs/GenerateReport.php
@@ -2,6 +2,7 @@
 
 namespace App\Jobs;
 
+use App\Models\Diagnose;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -17,7 +18,7 @@ class GenerateReport implements ShouldQueue
     /**
      * Create a new job instance.
      */
-    public function __construct(protected string $fileName, protected array $payloadObservations)
+    public function __construct(protected string $fileName, protected Diagnose $diagnoseModel)
     {
     }
 
@@ -26,24 +27,25 @@ class GenerateReport implements ShouldQueue
      */
     public function handle(): void
     {
-        if(count($this->payloadObservations) > 1) {
+        if(count($this->diagnoseModel->observations) > 1) {
             $this->sendForMultipleFiles();
         } else {
             $this->sendForSingleFile();
         }
+        $this->diagnoseModel->update(['report' => "reports/{$this->fileName}"]);
     }
 
     private function sendForMultipleFiles(): void
     {
         $response = Http::timeout(10000)
-            ->post(env('PROCESS_SERVER') . '/full_report', $this->payloadObservations);
+            ->post(env('PROCESS_SERVER') . '/full_report', $this->diagnoseModel->observations);
         Storage::disk('public')->put("reports/{$this->fileName}", $response->body());
     }
 
     private function sendForSingleFile(): void
     {
         $response = Http::timeout(10000)
-            ->post(env('PROCESS_SERVER') . '/report', $this->payloadObservations);
+            ->post(env('PROCESS_SERVER') . '/report', $this->diagnoseModel->observations);
         Storage::disk('public')->put("reports/{$this->fileName}", $response->body());
     }
 }

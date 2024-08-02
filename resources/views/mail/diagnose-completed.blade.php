9bac9e7247d544f52c129846794b311d22ef0c80
Omar Elewa <omarelewa25@gmail.com>
2024-08-03 00:36:19 +0300

    make the mail notify

---
+++ resources/views/mail/diagnose-completed.blade.php
@@ -0,0 +1,7 @@
+<x-mail::message>
+
+The new set of files have been diganosed and the results are ready for you to view in the [dashboard]({{ route('dashboard') }}).
+
+Thanks,<br>
+{{ config('app.name') }}
+</x-mail::message>

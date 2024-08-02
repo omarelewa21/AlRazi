e43de1052877e922f4e4bc52bd1cd2eef22218df
Omar Elewa <omarelewa25@gmail.com>
2024-08-03 00:36:57 +0300

    add a button for quick upload

--- resources/views/livewire/pages/image-diagnose/diagnosis-list.blade.php
+++ resources/views/livewire/pages/image-diagnose/diagnosis-list.blade.php
@@ -1,9 +1,15 @@
 <div>
     <div class="flex flex-row justify-between mb-4">
-        <h2 class="text-2xl font-medium">Worklist</h2>
-        <a href="{{ route('diagnose.create') }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            {{ __("Create New Diagnose") }}
-        </a>
+        <h2 class="text-2xl font-medium">@lang('Diagnose Worklist')</h2>
+        <div>
+            <button @click="window.open('{{ route('diagnose.create') }}', '_blank')" class="text-base bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                @lang('New Case')
+            </button>
+            <button class="text-base bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-4 rounded" wire:click="$dispatch('openModal', {component: 'pages.image-diagnose.quick-upload'})">
+                @lang("Quick Upload")
+            </button>
+        </div>
+
     </div>
 
 
